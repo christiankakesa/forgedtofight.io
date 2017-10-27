0a7a4d42bd9dae7f595feed36d587aa118bb5c14
Christian Kakesa <christian.kakesa@gmail.com>
2017-10-27 22:55:20 +0200

    Code review.

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
-require 'rack-timeout'
-
-Rack::Timeout::Logger.disable unless 'test'.eql?(ENV['RACK_ENV'])
+if 'test'.eql?(ENV['RACK_ENV'])
+  require 'rack-timeout'
+  Rack::Timeout::Logger.disable
+end
