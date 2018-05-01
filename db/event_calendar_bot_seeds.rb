26ed1712a2cb6039fe169ec7808d19236e43ae3a
Christian Kakesa <christian.kakesa@gmail.com>
2018-05-01 20:19:55 +0200

    Add new calendar bot for may 2018, Optimus Primal

--- db/event_calendar_bot_seeds.rb
+++ db/event_calendar_bot_seeds.rb
@@ -6,6 +6,12 @@
 [
   {
     type: :calendar_bot,
+    start_at: Time.parse('2018-04-30 17:00:00 PDT').utc,
+    image_url: nil,
+    bot_id: Bot.where(name: 'Optiminus Primal', type: :brawler).first.id
+  },
+  {
+    type: :calendar_bot,
     start_at: Time.parse('2018-03-31 17:00:00 PDT').utc,
     image_url: nil,
     bot_id: Bot.where(name: 'Megatron', type: :tactician).first.id
@@ -14,31 +20,31 @@
     type: :calendar_bot,
     start_at: Time.parse('2017-11-30 17:00:00 PDT').utc,
     image_url: nil,
-    bot_id: Bot.where(name: 'Ramjet').first.id
+    bot_id: Bot.where(name: 'Ramjet', type: :demolitions).first.id
   },
   {
     type: :calendar_bot,
     start_at: Time.parse('2017-10-31 17:00:00 PDT').utc,
     image_url: nil,
-    bot_id: Bot.where(name: 'Grimlock').first.id
+    bot_id: Bot.where(name: 'Grimlock', type: :brawler).first.id
   },
   {
     type: :calendar_bot,
     start_at: Time.parse('2017-09-30 17:00:00 PDT').utc,
     image_url: nil,
-    bot_id: Bot.where(name: 'Starscream').first.id
+    bot_id: Bot.where(name: 'Starscream', type: :tactician).first.id
   },
   {
     type: :calendar_bot,
     start_at: Time.parse('2017-08-31 17:00:00 PDT').utc,
     image_url: nil,
-    bot_id: Bot.where(name: 'Megatron').first.id
+    bot_id: Bot.where(name: 'Megatron', type: :demolitions).first.id
   },
   {
     type: :calendar_bot,
     start_at: Time.parse('2017-07-31 17:00:00 PDT').utc,
     image_url: nil,
-    bot_id: Bot.where(name: 'Hot Rod').first.id
+    bot_id: Bot.where(name: 'Hot Rod', type: :warrior).first.id
   }
 ].each do |event_calendar_bot|
   Event.upsert! event_calendar_bot
