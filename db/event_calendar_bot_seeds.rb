6f1e249257f375ed74f8a3cfd95407e86acd88a4
Christian Kakesa <christian.kakesa@gmail.com>
2018-07-22 05:26:33 +0200

    Update the calendar bot

--- db/event_calendar_bot_seeds.rb
+++ db/event_calendar_bot_seeds.rb
@@ -6,6 +6,12 @@
 [
   {
     type: :calendar_bot,
+    start_at: Time.parse('2018-06-30 17:00:00 PDT').utc,
+    image_url: nil,
+    bot_id: Bot.where(name: 'Kickback', type: :scout).first.id
+  },
+  {
+    type: :calendar_bot,
     start_at: Time.parse('2018-05-31 17:00:00 PDT').utc,
     image_url: nil,
     bot_id: Bot.where(name: 'Hound', type: :warrior).first.id
