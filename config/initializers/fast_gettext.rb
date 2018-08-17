5626a17a64e163047146ad7a61006bf97a6e4455
Christian Kakesa <christian.kakesa@gmail.com>
2018-08-18 01:28:41 +0200

    Fix po and mo generated filenames

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -3,6 +3,9 @@
 require 'fast_gettext'
 require 'gettext/mote_parser'
 
+# Fix the im-config.po generated filename
+ENV['TEXTDOMAIN'] = 'forgedtofightio' unless 'forgedtofightio'.eql?(ENV['TEXTDOMAIN'])
+
 Object.send(:include, FastGettext::Translation)
 locales_dir = File.join(File.dirname(__FILE__), '..', 'locales')
-FastGettext.add_text_domain('forgedtofightio', path: locales_dir, type: :po)
+FastGettext.add_text_domain(ENV['TEXTDOMAIN'], path: locales_dir, type: :po)
