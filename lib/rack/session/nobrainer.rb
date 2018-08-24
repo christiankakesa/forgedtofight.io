19ef66e39a7d0c45a187c3327b1e0eb849b81cbc
Christian Kakesa <christian.kakesa@gmail.com>
2018-08-25 01:15:11 +0200

    Review Rack::Session::NoBrainer

--- lib/rack/session/nobrainer.rb
+++ lib/rack/session/nobrainer.rb
@@ -17,8 +17,6 @@ module Rack
     end
 
     class NoBrainer < Abstract::Persisted
-      #DEFAULT_OPTIONS = Abstract::Persisted::DEFAULT_OPTIONS.merge drop: true
-
       def initialize(app, options = {})
         super
         @expire_after = options.fetch(:expire_after, 24 * 60 * 60)
@@ -27,13 +25,13 @@ module Rack
       def generate_sid
         loop do
           sid = super
-          break sid unless _exists?(sid)
+          break sid unless _get(sid)
         end
       end
 
-      def find_session(env, sid)
+      def find_session(_env, sid)
         sid ||= generate_sid
-        session_data = _exists?(sid)&.data
+        session_data = _get(sid)&.data
         unless session_data
           session_data = {}
           _set(sid, session_data)
@@ -41,26 +39,26 @@ module Rack
         [sid, session_data]
       end
 
-      def write_session(env, sid, session_data, options)
+      def write_session(_env, sid, session_data, _options)
         _set(sid, session_data) ? sid : false
       end
 
-      def delete_session(env, sid, options)
-        _exists?(sid)&.destroy
+      def delete_session(_env, sid, options)
+        _get(sid)&.destroy
         generate_sid unless options[:drop]
       end
 
       private
 
       def _set(sid, session_data)
-        model = _exists?(sid) || NoBrainerSessionStore.new(
+        model = _get(sid) || NoBrainerSessionStore.new(
           sid: sid,
           expires_at: RethinkDB::RQL.new.now + @expire_after
         )
         model.update(data: session_data)
       end
 
-      def _exists?(sid)
+      def _get(sid)
         NoBrainerSessionStore.where(sid: sid).first
       end
     end
