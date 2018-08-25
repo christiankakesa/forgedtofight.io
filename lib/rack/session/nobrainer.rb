d8756c47ce4dce87802aefa7ae063114e2f0619d
Christian Kakesa <christian.kakesa@gmail.com>
2018-08-25 06:22:46 +0200

    Remove distributed lock  and NoBrainer::Document::Timestamps

--- lib/rack/session/nobrainer.rb
+++ lib/rack/session/nobrainer.rb
@@ -8,7 +8,6 @@ module Rack
     # A NoBrainer document model for storing session data
     class NoBrainerSessionStore
       include NoBrainer::Document
-      include NoBrainer::Document::Timestamps
 
       field :sid, type: Text, unique: true
       field :data, type: Hash, default: {}
@@ -31,29 +30,23 @@ module Rack
       end
 
       def find_session(_env, sid)
-        ::NoBrainer::Lock.new('Rack::Session::NoBrainer').synchronize do
-          sid ||= generate_sid
-          session_data = _get(sid)&.data
-          puts "[1] #{session_data.inspect}"
-          unless session_data
-            session_data = {}
-            _set(sid, session_data)
-          end
-          puts "[2] #{session_data.inspect}"
-          [sid, session_data]
+        sid ||= generate_sid
+        session_data = _get(sid)&.data
+        puts "[1] #{session_data.inspect}"
+        unless session_data
+          session_data = {}
+          _set(sid, session_data)
         end
+        puts "[2] #{session_data.inspect}"
+        [sid, session_data]
       end
 
       def write_session(_env, sid, session_data, _options)
-        ::NoBrainer::Lock.new('Rack::Session::NoBrainer').synchronize do
-          _set(sid, session_data) ? sid : false
-        end
+        _set(sid, session_data) ? sid : false
       end
 
       def delete_session(_env, sid, options)
-        ::NoBrainer::Lock.new('Rack::Session::NoBrainer').synchronize do
-          _get(sid)&.destroy
-        end
+        _get(sid)&.destroy
         generate_sid unless options[:drop]
       end
 
