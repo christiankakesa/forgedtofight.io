32e9919fd228d1bc114877c4af1fa0fe1d347c00
Christian Kakesa <christian.kakesa@gmail.com>
2018-09-12 01:40:09 +0200

    Add gravatar for user's picture

---
+++ helpers/gravatar_helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'digest/md5'
+
+module Ftf
+    module Helpers
+        class Gravatar
+            def self.get(email, size = 128, rating = 'g', default = 'mp')
+                res = +'//www.gravatar.com/avatar/'
+                res << Digest::MD5.hexdigest(email)
+                res << "?s=#{size}&r=#{rating}&d=#{default}"
+                res
+            rescue StandardError
+                "//www.gravatar.com/avatar?s=#{size}&r=#{rating}&d=#{default}"
+            end
+        end
+    end
+end
