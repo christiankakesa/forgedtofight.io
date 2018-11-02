74210a291d3d6f60fc708ef7ff7300d208f67a44
Christian Kakesa <christian.kakesa@gmail.com>
2018-11-02 04:56:03 +0100

    Add upcoming bot Scorponok

--- db/event_upcoming_bot_seeds.rb
+++ db/event_upcoming_bot_seeds.rb
@@ -6,6 +6,12 @@
 [
   {
     type: :upcoming_bot,
+    start_at: Time.parse('2018-11-01 10:00:00 PDT').utc,
+    image_url: nil,
+    bot_id: Bot.where(name: 'Scorponok', type: :warrior).first.id
+  },
+  {
+    type: :upcoming_bot,
     start_at: Time.parse('2018-10-04 10:00:00 PDT').utc,
     image_url: nil,
     bot_id: Bot.where(name: 'Megatronus', type: :demolitions).first.id
